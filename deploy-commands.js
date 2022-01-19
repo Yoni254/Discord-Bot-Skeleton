5e8dbf640450c9887f6fe699cb9c20dd73ff24d9
Yoni254 <tzukermany@gmail.com>
2022-01-19 02:07:40 +0200

    initial setup

---
+++ deploy-commands.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./configs/config.json');
+
+const commands = []
+
+
+const adminFiles = fs.readdirSync('./commands/admin').filter(file => file.endsWith('.js'));
+const devFiles = fs.readdirSync('./commands/dev').filter(file => file.endsWith('.js'));
+const modFiles = fs.readdirSync('./commands/mod').filter(file => file.endsWith('.js'));
+const userFiles = fs.readdirSync('./commands/user').filter(file => file.endsWith('.js'));
+
+function loadCommand(path, dir) {
+    for (const file of dir) {
+        const command = require(`${path}/${file}`);
+        commands.push(command.data.toJSON());
+    }
+}
+loadCommand('./commands/admin', adminFiles)
+loadCommand('./commands/dev', devFiles)
+loadCommand('./commands/mod', modFiles)
+loadCommand('./commands/user', userFiles)
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
---
+++ index.js
@@ -0,0 +1,52 @@
+// Require the necessary discord.js classes
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./configs/config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
+
+const adminFiles = fs.readdirSync('./commands/admin').filter(file => file.endsWith('.js'));
+const devFiles = fs.readdirSync('./commands/dev').filter(file => file.endsWith('.js'));
+const modFiles = fs.readdirSync('./commands/mod').filter(file => file.endsWith('.js'));
+const userFiles = fs.readdirSync('./commands/user').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+function loadCommand(path, dir) {
+    for (const file of dir) {
+        const command = require(`${path}/${file}`);
+        client.commands.set(command.data.name, command);
+    }
+}
+
+loadCommand('./commands/admin', adminFiles)
+loadCommand('./commands/dev', devFiles)
+loadCommand('./commands/mod', modFiles)
+loadCommand('./commands/user', userFiles)
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+
+// Login to Discord with your client's token
+client.login(token);
